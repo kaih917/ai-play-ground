ffea7e8a28f0780b2d464124b82c187b02d6a705
kaih917 <kai_h_917@msn.com>
2026-08-02 10:25:39 +0800

    remove useless prints

--- src/exceptions/exceptions.py
+++ src/exceptions/exceptions.py
@@ -12,7 +12,4 @@ class ApiException(Exception):
     def __init__(self, status_code: int, message: str = None):
         self.status_code = status_code
         self.message = message or status_codes.get(status_code, "Unknown Error")
-        print('test commit 1')
-        print('test commit 2')
-        # test commit 3
         super().__init__(self.message)
